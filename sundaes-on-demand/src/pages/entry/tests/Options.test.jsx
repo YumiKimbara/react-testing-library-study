da72f9e1e27ce4883a1fd574877fcd87ee72cc76
Yumi <a13158y@gmail.com>
2023-05-02 13:10:59 -0700

    test code for toppings

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
+/*
+ * test for icecream
+ */
 test("displays image for each scoop option from server", async () => {
   // send props
   render(<Options optionType="scoops" />);
@@ -11,4 +14,25 @@ test("displays image for each scoop option from server", async () => {
 
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+/*
+ * test for icecream toppings
+ */
+test("Displays image for each toppings option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const imageTitles = toppingImages.map((element) => {
+    return element.alt;
+  });
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
